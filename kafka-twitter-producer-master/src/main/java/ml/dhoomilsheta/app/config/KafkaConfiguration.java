4d12aebc6bf1974f199d29ac3ed2170fe6494aa7
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-06-10 00:59:10 -0500

    BDt project

---
+++ kafka-twitter-producer-master/src/main/java/ml/dhoomilsheta/app/config/KafkaConfiguration.java
@@ -0,0 +1,7 @@
+package ml.dhoomilsheta.app.config;
+
+public class KafkaConfiguration {
+    public static final String SERVERS = "69.18.37.194:9092";
+    public static final String TOPIC = "bdtProject";
+    public static final long SLEEP_TIMER = 1000;
+}
---
+++ kafka-twitter-producer-master/src/main/java/ml/dhoomilsheta/app/config/TwitterConfiguration.java
@@ -0,0 +1,9 @@
+package ml.dhoomilsheta.app.config;
+
+public class TwitterConfiguration {
+    public static final String CONSUMER_KEY = "UKJVJZQEUsfDSUGHmNH1EXVGC";
+    public static final String CONSUMER_SECRET = "lR6rG592nhCpl29yWm3XRS5Kdr8YcP4LlStEQZU53D4C9mfNVO";
+    public static final String ACCESS_TOKEN = "1270223066723991552-VPneCS51oidFEF7g57AyusYAyngmIr";
+    public static final String TOKEN_SECRET = "65yfMo6V2FD8Wr7xJyT05iZrvpfJRDHsYDOFuBfxgUsEc";
+    public static final String HASHTAG = "#trump";
+}
